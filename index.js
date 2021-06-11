cb2b9cfa5978b199b107b50e637a6c2ea9299956
yuno <hanganda23@naver.com>
2021-06-11 17:42:01 +0900

    add timer

---
+++ index.js
@@ -0,0 +1,74 @@
+const timer = document.getElementById('timer');
+const lines = timer.querySelector('#lines');
+const fins = timer.querySelector('#fins');
+const nums = timer.querySelector('#num-container');
+
+const endTime = 40;
+
+function paintTick() {
+    for(let i=0; i<30; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.transform = `rotate(${i*6}deg)`;
+
+        if (i%5 == 0) {
+            line.classList.add('thick')
+        }
+
+        lines.append(line);
+    }
+}
+
+function paintNumber() {
+    let left = 15;
+    let right = 45;
+
+    for (let i=0; i<6; i++) {
+        const numBox = document.createElement('div');
+        numBox.classList.add('num-box');
+        numBox.style.transform = `rotate(${i*30}deg)`;
+
+        const spanLeft = document.createElement('span');
+        const spanRight = document.createElement('span');
+
+        const leftText = left - 5*i;
+        spanLeft.textContent = leftText<0 ? 60+ leftText : leftText;
+        spanRight.textContent = right - (5 * i);
+
+        spanLeft.style.transform = `rotate(${-30*i}deg)`;
+        spanRight.style.transform = `rotate(${-30*i}deg)`;
+
+        numBox.append(spanLeft,spanRight);
+        nums.append(numBox);
+    }
+}
+
+function paintRemainTime() {
+    for (let min=0; min<endTime; min++) {
+        for (let sec=0; sec<60; sec++) {
+            const remainFin = document.createElement('div');
+            remainFin.classList.add('fin');
+
+            const deg = min*6+sec*0.1;
+            remainFin.style.transform = `rotate(${-deg}deg)`
+            
+            fins.append(remainFin);
+        }
+    }
+}
+
+function tickSec() {
+    const lastFin = fins.lastChild;
+    if (lastFin) {
+        lastFin.remove();
+    }
+}
+
+function play() {
+    setInterval(tickSec,1000)
+}
+
+paintTick();
+paintNumber();
+paintRemainTime();
+play();
